208cb4df03e80e170714a2dc3dc04681153d8264
Ddemkowicz <dakucp@gmail.com>
2023-03-13 21:07:27 +0100

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,14 +16,14 @@ const form = document.querySelector('form');
 form.addEventListener(`submit`, e => {
   e.preventDefault();
 
-  const delayInput = document.querySelector('input[name="delay"]').value;
-  const stepInput = document.querySelector('input[name="step"]').value;
-  const amountInput = document.querySelector('input[name="amount"]').value;
+  const delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
 
-  for (let i = 1; i <= amountInput; i++) {
-    const position = i;
-    const currentDelay = delayInput + stepInput;
-    createPromise(position, currentDelay)
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const promiseDelay = delay + i * step;
+    createPromise(position, promiseDelay)
       .then(({ position, delay }) => {
         console.log(`Fulfilled promise ${position} in ${delay}ms`);
       })
@@ -32,4 +32,3 @@ form.addEventListener(`submit`, e => {
       });
   }
 });
-// komentarz test
