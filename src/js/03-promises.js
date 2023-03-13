dc165b2447090499cd33fe1438e7e1df30320c83
Ddemkowicz <dakucp@gmail.com>
2023-03-13 16:30:38 +0100

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('form');
+
+form.addEventListener(`submit`, e => {
+  e.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]').value;
+  const stepInput = document.querySelector('input[name="step"]').value;
+  const amountInput = document.querySelector('input[name="amount"]').value;
+
+  for (let i = 1; i <= amountInput; i++) {
+    const position = i;
+    const currentDelay = delayInput + stepInput;
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
